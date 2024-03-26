c8df306be6ee7329308310fd46220c1e399d15c3
Paul-Fournet <144245169+Paul-Fournet@users.noreply.github.com>
2024-03-26 11:49:09 +0100

    php

--- test.php
+++ test.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="footer.css" />
+
+    <!--Importation des polices d'écriture-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Arimo&family=Sofia+Sans&display=swap"
+      rel="stylesheet"
+    />
 </head>
 <body>
 
 <?php
-    for($i=0;$i<10;$i+=1){
-        if($1%2===0){
-            echo '<p style="color:red;">'.$i.'</p>';
-        }
-        else{
-            echo '<p style="color:blue;">'.$i.'</p>';
-        }
-    }
+
 ?>
 
 
