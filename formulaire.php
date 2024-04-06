665996a9caca4183f987bc02fe9870a447f66c45
Paul-Fournet <144245169+Paul-Fournet@users.noreply.github.com>
2024-04-06 22:10:51 +0200

    test

--- formulaire.php
+++ formulaire.php
@@ -1,7 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se connecter</title>
+    <link rel="stylesheet" href="formstyle.css" />
+
+</head>
+
+<body>
+
+    <div class="boutonretour">
+        <a href="index.html">
+            Retour à l'accueil
+        </a>
+    </div>
+
+    <div class="divform">
+        
+        
+        <form class="formulaire" method="post" action="formulaire.php">
+            <p class="titre">Connexion</p><br>
+    
+            <label>Identifiant</label><br><input type="text" name="id" placeholder="Votre identifiant" class="inputtext"><br><br>
+
+            <label>Mot de passe</label><br><input type="password" name="mdp" class="inputtext" placeholder="Votre mot de passe"><br><br>
+
+            <input type="submit" name="submit">
+    
+        </form>
+        
 <?php
 
+
 if(isset($_POST['submit'])){
-    header("Location:https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    echo $_POST['mdp'];
 }
 
+
+
+
 ?>
+    </div>
+
+
+</body>
+
+</html>
