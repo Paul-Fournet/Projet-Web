4901413b0fd938d1153bf1704e3664c3e2482387
Paul-Fournet <144245169+Paul-Fournet@users.noreply.github.com>
2024-04-08 08:13:15 +0200

    chgt couleur bouton retour

--- pagereportclient.php
+++ pagereportclient.php
@@ -24,45 +24,7 @@ if($_SESSION['connected']===false){
 </body>
 
 
-<footer id="scrollfooter">
-    <div class="contacter">
-      <h2>Nous contacter</h2>
-      <ul>
-        <li><p>E-mail : contact@planet-discovery.com</p></li>
-        <li><p>Téléphone : 06 54 32 10 12</p></li>
-
-        <?php
-          if($_SESSION['user']==='client'){
-        ?>
-        <li><a href="pagereportclient.php">Un problème ? / Une suggestion ?</a></li>
-        <?php
-          }
-          
-          elseif($_SESSION['user']==='admin'){
-        ?>
-        <li><a href="pagereportadmin.php">Reports de bugs</a></li>
-        <?php
-          }
-        ?>
-      </ul>
-    </div>
-    <div class="legal">
-      <h2>Mentions légales</h2>
-      <ul>
-        <li><a href="#">Conditions générales d’utilisation</a></li>
-        <li><a href="#">Conditions Générales de Vente</a></li>
-        <li><a href="#">Politique de Confidentialité</a></li>
-        
-      </ul>
-    </div>
-    <div class="footer-col">
-      <h2>Réseaux Sociaux</h2>
-      <div class="links">
-        <a href="#" class="icon"><i class="fab fa-linkedin-in"></i></a>
-        <a href="#" class="icon"><i class="fab fa-facebook-f"></i></a>
-        <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
-        <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
-      </div>
-    </div>
-  </footer>
+<?php
+require("indexparts/footer.php");
+?>
 </html>
